4f21eb161e2c5b98ad5d4b49d40ccd12c7b750b4
TheAustinMiller <themilleraustin@gmail.com>
2025-09-19 17:36:54 -0500

    More Front end work

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  currentTime: string = '';
+  showTime: boolean = true;
+  private timeInterval: any;
 
+  ngOnInit(): void {
+    this.updateTime();
+    // Update time every second
+    this.timeInterval = setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timeInterval) {
+      clearInterval(this.timeInterval);
+    }
+  }
+
+  private updateTime(): void {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true
+    });
+  }
+
+  toggleDisplay(): void {
+    this.showTime = !this.showTime;
+  }
 }
